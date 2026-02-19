87f2e2611bfde8d0da14a7d61507f37d50afefbe
sundoa <vihaan.patel.16.july@gmail.com>
2026-02-19 17:16:11 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,14 @@
+export default function Navbar({ downloadFile }) {
+  const user = localStorage.getItem("vellumUser");
+
+  return (
+    <div className="navbar">
+      <div>Vellum</div>
+      <div>
+        <span>{user}</span>
+        <button onClick={downloadFile}>Download</button>
+      </div>
+    </div>
+  );
+}
 
