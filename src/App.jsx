110679127891d0f0beeb3c27ca4dc36d7ec1066c
sundoa <vihaan.patel.16.july@gmail.com>
2026-02-19 17:51:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 import Editor from "./pages/Editor";
 
 export default function App() {
@@ -9,9 +10,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/editor" element={<Editor />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
