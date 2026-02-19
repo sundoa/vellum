6a6377554e145f8ad05fa2af7f0442bd217469bd
sundoa <vihaan.patel.16.july@gmail.com>
2026-02-19 17:16:38 +0530

    Update CustomContextMenu.jsx

--- src/components/CustomContextMenu.jsx
+++ src/components/CustomContextMenu.jsx
@@ -1,1 +1,23 @@
+export default function CustomContextMenu({ x, y, close, editor }) {
+  return (
+    <div
+      className="context-menu"
+      style={{ top: y, left: x }}
+      onMouseLeave={close}
+    >
+      <div onClick={() => editor.chain().focus().toggleBold().run()}>
+        Bold
+      </div>
+      <div onClick={() => editor.chain().focus().toggleItalic().run()}>
+        Italic
+      </div>
+      <div onClick={() => editor.chain().focus().toggleBulletList().run()}>
+        Bullet List
+      </div>
+      <div onClick={() => editor.chain().focus().sinkListItem("listItem").run()}>
+        Indent
+      </div>
+    </div>
+  );
+}
 
