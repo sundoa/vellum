df6e5648934a891e4575bb95ac56d5b0f17d96f3
sundoa <vihaan.patel.16.july@gmail.com>
2026-02-19 20:26:54 +0530

    Update Editor.jsx

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,37 +1,52 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { useState, useContext } from "react";
-import CustomContextMenu from "../components/CustomContextMenu";
+import { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
-import { AppContext } from "../context/AppContext";
+import CustomContextMenu from "../components/CustomContextMenu";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { useLocation } from "react-router-dom";
 
 export default function Editor() {
+  const { user } = useContext(AppContext);
+  const location = useLocation();
+  const folder = location.state?.folder;
+  const [currentDoc, setCurrentDoc] = useState({ name: "Untitled", content: "" });
   const [menu, setMenu] = useState(null);
-  const { activeFolder } = useContext(AppContext);
 
   const editor = useEditor({
     extensions: [StarterKit],
-    content: "<p>Start writing...</p>",
+    content: currentDoc.content || "<p>Start writing...</p>",
+    onUpdate: async ({ editor }) => {
+      const updatedContent = editor.getHTML();
+      setCurrentDoc(prev => ({ ...prev, content: updatedContent }));
+
+      if (user && folder) {
+        const folderRef = doc(db, "users", user.uid, "folders", folder.id);
+        await setDoc(folderRef, {
+          ...folder,
+          docs: [{ name: currentDoc.name, content: updatedContent }],
+        });
+      }
+    },
   });
 
   const downloadFile = () => {
+    if (!editor) return;
     const blob = new Blob([editor.getText()], { type: "text/plain" });
     const link = document.createElement("a");
     link.href = URL.createObjectURL(blob);
-    link.download = `${activeFolder?.name || "vellum-document"}.txt`;
+    link.download = `${currentDoc.name}.txt`;
     link.click();
-  };
-
-  const handleFolderSelect = (folder) => {
-    // Optional: Switch active folder while editing
   };
 
   return (
     <>
       <Navbar downloadFile={downloadFile} />
       <div className="editor-page fade">
-        <Sidebar onSelectFolder={handleFolderSelect} />
+        <Sidebar onSelectFolder={() => {}} />
         <div
           className="editor-wrapper"
           onContextMenu={(e) => {
