34fd232e628ceed4d9b04dbea07b4307a8fc7692
sundoa <vihaan.patel.16.july@gmail.com>
2026-02-19 17:15:39 +0530

    Update Editor.jsx

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,1 +1,47 @@
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { useState } from "react";
+import CustomContextMenu from "../components/CustomContextMenu";
+import Navbar from "../components/Navbar";
+
+export default function Editor() {
+  const [menu, setMenu] = useState(null);
+
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Start writing...</p>",
+  });
+
+  const downloadFile = () => {
+    const blob = new Blob([editor.getText()], { type: "text/plain" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "vellum-document.txt";
+    link.click();
+  };
+
+  return (
+    <>
+      <Navbar downloadFile={downloadFile} />
+      <div
+        className="editor-wrapper"
+        onContextMenu={(e) => {
+          e.preventDefault();
+          setMenu({ x: e.pageX, y: e.pageY });
+        }}
+      >
+        <EditorContent editor={editor} className="editor" />
+
+        {menu && (
+          <CustomContextMenu
+            x={menu.x}
+            y={menu.y}
+            close={() => setMenu(null)}
+            editor={editor}
+          />
+        )}
+      </div>
+    </>
+  );
+}
 
